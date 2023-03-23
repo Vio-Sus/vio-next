c89548b0805aef4c96740d0dcd5299a5cc666705
Cocoandjane <janeliangbc@gmail.com>
2023-03-22 21:56:27 -0700

    p

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
 import { SessionProvider } from "next-auth/react";
-
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
- 
+ const router = useRouter()
+
   return (
     <SessionProvider session={pageProps.session}>
-      {Component.name === "SignIn" || Component.name === "SignUp" ? (
+      {router.pathname === '/auth/SignIn' || router.pathname ==='/auth/SignUp' ? (
         <Component {...pageProps} />
       ) : (
         <Layout session={pageProps.session}>
