c34352cae205d9f0debf448e5dce04efd84c8ed2
Cocoandjane <janeliangbc@gmail.com>
2023-04-08 22:06:20 -0700

    g

--- src/pages/api/deleteFileEntry.ts
+++ src/pages/api/deleteFileEntry.ts
@@ -10,24 +10,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  try {
-    const selectedData = req.body;
-    console.log(selectedData);
-    selectedData.map(async (m: number) => {
-      await prisma.entry.deleteMany({
-        where: {
-          entryFileId: m,
-        },
-      });
-      await prisma.entryFile.deleteMany({
-        where: {
-          id: m,
-        },
-      });
-    });
-    res.status(200).json(selectedData);
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+  // try {
+  //   const selectedData = req.body;
+  //   console.log(selectedData);
+  //   selectedData.map(async (m: number) => {
+  //     await prisma.entry.deleteMany({
+  //       where: {
+  //         entryFileId: m,
+  //       },
+  //     });
+  //     await prisma.entryFile.deleteMany({
+  //       where: {
+  //         id: m,
+  //       },
+  //     });
+  //   });
+  //   res.status(200).json(selectedData);
+  // } catch (error) {
+  //   res.status(500);
+  //   console.log(error);
+  // }
 }
